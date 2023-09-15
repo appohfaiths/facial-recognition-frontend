6425d8f0d9baf939e20dd99a3c35f779c4f7aa57
Faith Sromawuda Appoh <appohfaiths@gmail.com>
2023-09-15 12:58:09 +0000

    uploading photo name to backend

--- src/components/uploadPhoto/UploadPhoto.jsx
+++ src/components/uploadPhoto/UploadPhoto.jsx
@@ -2,16 +2,19 @@ import { useState } from "react";
 
 export const UploadPhoto = () => {
     const [selectedFile, setSelectedFile] = useState(null);
+    const [photoName, setPhotoName] = useState("");
     const [uploadedPhotoId, setUploadedPhotoId] = useState(null);
 
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
     };
 
-    const handleUpload = async () => {
+    const handleUpload = async (e) => {
         const formData = new FormData();
         formData.append("photo", selectedFile);
+        formData.append("photoName", photoName);
 
+        e.preventDefault();
         try {
             const response = await fetch("http://localhost:5273/upload-photo", {
                 method: "POST",
@@ -31,8 +34,12 @@ export const UploadPhoto = () => {
 
     return (
         <div>
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={handleUpload}>Upload Photo</button>
+            <form action="" encType="multipart/form-data">
+                <label htmlFor="photoName">Name</label>
+                <input type="text" onChange={(e) => setPhotoName(e.target.value)} />
+                <input type="file" onChange={handleFileChange} />
+                <button onClick={e => handleUpload(e)}>Upload Photo</button>
+            </form>
             {uploadedPhotoId && <p>Uploaded Photo ID: {uploadedPhotoId}</p>}
         </div>
     );
